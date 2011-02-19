fe36f9b69f2de0a0a057545acee6a924b67763f2
Sean Kelley <sp_kelley@yahoo.com>
2011-02-19 10:40:12 -0600

    initial commit of rails new command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GamerFind::Application.config.secret_token = '8062281f154655aa34ac63e75bfef293449b5b0b9cc1dbb5b884ec6c46b4278f2a9678cf3a1b8679b643e0765d7a4ccda4d510e2b669e0def45d99b364e8e1d2'
